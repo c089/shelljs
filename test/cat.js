f7e1e7ab44b7e9860f80a15a1a41da4026ad63d0
Christoph Neuroth <christoph.neuroth@gmail.com>
2015-03-18 10:50:58 +0100

    wrap each test in a mocha `it` block

--- test/cat.js
+++ test/cat.js
@@ -10,43 +10,48 @@ describe('cat', function () {
     shell.config.silent = true;
   });
 
-  it('all tests', function () {
+  describe('invalids', function () {
 
+    it('should fail without argument', function () {
+      shell.cat();
+      assert.ok(shell.error());
+    });
 
-    //
-    // Invalids
-    //
-
-    shell.cat();
-    assert.ok(shell.error());
-
-    assert.equal(fs.existsSync('/asdfasdf'), false); // sanity check
-    shell.cat('/adsfasdf'); // file does not exist
-    assert.ok(shell.error());
-
-    //
-    // Valids
-    //
-
-    // simple
-    var result = shell.cat('resources/file1');
-    assert.equal(shell.error(), null);
-    assert.equal(result, 'test1');
-
-    // multiple files
-    var result = shell.cat('resources/file2', 'resources/file1');
-    assert.equal(shell.error(), null);
-    assert.equal(result, 'test2\ntest1');
-
-    // multiple files, array syntax
-    var result = shell.cat(['resources/file2', 'resources/file1']);
-    assert.equal(shell.error(), null);
-    assert.equal(result, 'test2\ntest1');
-
-    var result = shell.cat('resources/file*.txt');
-    assert.equal(shell.error(), null);
-    assert.ok(result.search('test1') > -1); // file order might be random
-    assert.ok(result.search('test2') > -1);
+    it('should fail when file does not exist', function () {
+      assert.equal(fs.existsSync('/asdfasdf'), false); // sanity check
+      shell.cat('/adsfasdf'); // file does not exist
+      assert.ok(shell.error());
+    });
 
   });
+
+  describe('valids', function () {
+
+    it('simple', function () {
+      var result = shell.cat('resources/file1');
+      assert.equal(shell.error(), null);
+      assert.equal(result, 'test1');
+    });
+
+    it('multiple files', function () {
+      var result = shell.cat('resources/file2', 'resources/file1');
+      assert.equal(shell.error(), null);
+      assert.equal(result, 'test2\ntest1');
+    });
+
+    it('multiple files, array syntax', function () {
+      var result = shell.cat(['resources/file2', 'resources/file1']);
+      assert.equal(shell.error(), null);
+      assert.equal(result, 'test2\ntest1');
+    });
+
+    it('should cat multiple files when using a wildcard', function () {
+      var result = shell.cat('resources/file*.txt');
+      assert.equal(shell.error(), null);
+      assert.ok(result.search('test1') > -1); // file order might be random
+      assert.ok(result.search('test2') > -1);
+    });
+
+  });
+
 });
