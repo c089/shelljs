910f6614758906fdcf8902c87a6215dfadbc4885
Christoph Neuroth <christoph.neuroth@gmail.com>
2015-03-18 10:43:36 +0100

    reformat

--- test/cat.js
+++ test/cat.js
@@ -1,48 +1,48 @@
 var shell = require('..');
 
 var assert = require('assert'),
-    fs = require('fs');
+  fs = require('fs');
 
-describe('cat', function() {
-  
-  beforeEach(function() {
-    shell.cd(__dirname);    
+describe('cat', function () {
+
+  beforeEach(function () {
+    shell.cd(__dirname);
     shell.config.silent = true;
   });
-  
-  it('all tests', function() {
-        
-    
+
+  it('all tests', function () {
+
+
     //
     // Invalids
     //
-    
+
     shell.cat();
     assert.ok(shell.error());
-    
+
     assert.equal(fs.existsSync('/asdfasdf'), false); // sanity check
     shell.cat('/adsfasdf'); // file does not exist
     assert.ok(shell.error());
-    
+
     //
     // Valids
     //
-    
+
     // simple
     var result = shell.cat('resources/file1');
     assert.equal(shell.error(), null);
     assert.equal(result, 'test1');
-    
+
     // multiple files
     var result = shell.cat('resources/file2', 'resources/file1');
     assert.equal(shell.error(), null);
     assert.equal(result, 'test2\ntest1');
-    
+
     // multiple files, array syntax
     var result = shell.cat(['resources/file2', 'resources/file1']);
     assert.equal(shell.error(), null);
     assert.equal(result, 'test2\ntest1');
-    
+
     var result = shell.cat('resources/file*.txt');
     assert.equal(shell.error(), null);
     assert.ok(result.search('test1') > -1); // file order might be random
